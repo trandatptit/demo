ae2dc74d1d1e9c1cad7f341100c35ecc39c1d4f5
trandatptit <trantiendat6112k3@gmail.com>
2025-11-02 20:21:43 +0700

    new PopupPackage and PopupNatriRoadmap

---
+++ client/src/views/TestPopup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center gap-4">
+    <Button label="Open PopupPackage" @click="showPopup = true" />
+
+    <!-- PopupPackage -->
+    <PopupPackage v-model="showPopup" />
+
+    <Button label="Open PopupNatriRoadmap" @click="showNatriRoadmap = true" />
+
+    <!-- PopupNatriRoadmap -->
+    <PopupNatriRoadmap v-model="showNatriRoadmap" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Button from "primevue/button";
+import PopupPackage from "../components/popup/PopupPackage.vue";
+import PopupNatriRoadmap from "../components/popup/PopupNatriRoadmap.vue";
+
+const showPopup = ref(false);
+const showNatriRoadmap = ref(false);
+</script>
